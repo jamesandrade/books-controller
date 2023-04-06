5ba25ddba713560524d5f83e4bca0bb2ee3c17b3
Jameson Andrade <jameson_andrade@hotmail.com.br>
2023-04-05 22:39:47 -0300

    fix set timeout logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -22,20 +22,21 @@ function Sidebar() {
 
   const handleExit = (event: any) => {
     event.preventDefault()
+    toast.success('êxito!', {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
     setTimeout(() => {
-      toast.success('êxito!', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }, 5000);
-    localStorage.removeItem('token')
-    navigate('/');
+      localStorage.removeItem('token')
+      navigate('/');
+    },2000);
+
   };
 
   return (
